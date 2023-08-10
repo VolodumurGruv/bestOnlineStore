17eed8139722e28b97dae2bc09a74d7ef1d1d345
VolodumurGrushka <gru.v@i.ua>
2023-08-10 23:26:19 +0300

    added menu and routing

---
+++ front-end/src/app/categories/gardening/gardening.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gardening',
+  templateUrl: './gardening.component.html',
+  styleUrls: ['./gardening.component.scss']
+})
+export class GardeningComponent {
+
+}
