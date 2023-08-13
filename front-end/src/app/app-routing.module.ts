e7be2b946b8f2a3c421f6af8c9d89ffdada01d56
VolodumurGrushka <gru.v@i.ua>
2023-08-13 22:40:08 +0300

    updated

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home-page/home/home.component';
+import { LayoutComponent } from './shared/layout/layout.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: LayoutComponent, children: [
+    {path: '', component: HomeComponent},
+    {path: '', redirectTo: '/', pathMatch: 'full'},
+  ]},
+  {path: '**', redirectTo: '/', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './shared/layout/layout.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './home-page/home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
