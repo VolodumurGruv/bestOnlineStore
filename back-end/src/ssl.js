3893c5992d4e777eab73100959a1c1ceca6f9451
MarkAlexI <markovaleksandr108@gmail.com>
2023-08-14 02:03:15 +0300

    Fixed routes.

--- back-end/src/ssl.js
+++ back-end/src/ssl.js
@@ -1,40 +1,40 @@
 import express from 'express';
 import { fileURLToPath } from 'url'
+import fs from 'fs';
+import https from 'https';
 import path from 'path';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routers/user.js';
-//import User from './models/user.js';
-//import data from './data.js';
-import isAdmin from './utils/auth.js';
 
 dotenv.config({ path: '../.env' });
 
-const port = 30000;
+const port = 1337;
 const app = express();
 const uri = `mongodb+srv://team:${process.env.MONGODB_PASSWORD}@cluster0.qfcqvtb.mongodb.net/?retryWrites=true&w=majority`;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+/*
+app.use('*', (req, res) => {
+  console.log(req.originalUrl);
+});
+*/
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use('*', (req, res, next) => {
-  console.log(req.originalUrl);
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  next();
-});
-
-app.use('/api/users', userRouter);
 
 const staticPath = path.join(__dirname, '../../front-end/dist/front-end/');
 app.use(express.static(staticPath));
 
 app.get('/favicon.ico', (req, res) => {
-  res.sendFile(path.join(staticPath, 'favicon.ico'));
+  res.sendFile(join(staticPath, 'favicon.ico'));
+});
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
 });
 
 mongoose.connect(uri, {})
@@ -55,7 +55,9 @@ dbConnection
     console.log("Connected to DB!");
   });
 
-
+app.use('/api/users', userRouter);
+app.use('/api/seed', userRouter);
+/*
 app.get('*', (req, res) => {
   console.log(JSON.stringify(req.headers));
   res.sendFile(path.join(staticPath, 'index.html'));
@@ -63,4 +65,14 @@ app.get('*', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server at http://localhost:${port}`);
+});
+*/
+const options={
+  key: fs.readFileSync(path.join(__dirname,'../cert/key.pem')),
+  cert: fs.readFileSync(path.join(__dirname,'../cert/cert.pem'))
+};
+
+const sslServer=https.createServer(options,app);
+sslServer.listen(1337, () => {
+  console.log('Secure server is listening on port 1337');
 });
