19e4217662f280ef5f30b35e4f00d3dc1559de95
VolodumurGrushka <gru.v@i.ua>
2023-08-15 23:16:37 +0300

    main done with styling for now

---
+++ front-end/src/app/home-page/components/advices/advices.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-advices',
+  templateUrl: './advices.component.html',
+  styles: [
+  ]
+})
+export class AdvicesComponent {
+
+}
