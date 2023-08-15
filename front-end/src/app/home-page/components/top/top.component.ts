5439f8ccc6a1fcfcfb1310a1adcf99832984079e
VolodumurGrushka <gru.v@i.ua>
2023-08-15 16:16:04 +0300

    top products section at the beginning

--- front-end/src/app/home-page/components/top/top.component.ts
+++ front-end/src/app/home-page/components/top/top.component.ts
@@ -3,9 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-top',
   templateUrl: './top.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class TopComponent {
-
+  public mockCardData = [
+    { title: 'Касовий столик', price: 6500, img: '', discount: 0 },
+    { title: 'Касовий столик', price: 5500, img: '', discount: 6500 },
+    { title: 'Касовий столик', price: 5000, img: '', discount: 6500 },
+  ];
 }
