585849e220f905e708059b68927a7cca2cd1bb9e
shytuleva24 <shutyleva@gmail.com>
2023-08-18 21:00:50 +0300

    some styling tweaks added a hover and an active link header

--- front-end/src/app/catalog/categories/categories.component.ts
+++ front-end/src/app/catalog/categories/categories.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { mainCategories } from "../../interfaces/catalog.data";
+import { Category } from "../../interfaces/catalog.interface";
 
 @Component({
   selector: 'app-categories',
@@ -9,4 +11,14 @@ import { RouterLink } from '@angular/router';
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.scss'],
 })
-export class CategoriesComponent {}
+export class CategoriesComponent {
+  categories: Category[] = mainCategories;
+
+  showSubcategories(category: Category) {
+    category.showSubcategories = true;
+  }
+
+  hideSubcategories() {
+    this.categories.forEach(category => category.showSubcategories = false);
+  }
+}
