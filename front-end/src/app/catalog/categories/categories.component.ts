c62f8b113329ae8713caa429699b7fd3f6e558e0
VolodumurGrushka <gru.v@i.ua>
2023-08-18 15:40:11 +0300

    starting point

--- front-end/src/app/catalog/categories/categories.component.ts
+++ front-end/src/app/catalog/categories/categories.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.scss']
+  styleUrls: ['./categories.component.scss'],
 })
-export class CategoriesComponent {
-
-}
+export class CategoriesComponent {}
