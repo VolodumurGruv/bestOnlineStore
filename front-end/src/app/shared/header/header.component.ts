0f066c01d49f5fd862a2176a1bfebf66cd60c861
shytuleva24 <shutyleva@gmail.com>
2023-08-18 21:00:50 +0300

    some styling tweaks added a hover and an active link header

--- front-end/src/app/shared/header/header.component.ts
+++ front-end/src/app/shared/header/header.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchComponent } from './components/search/search.component';
-import { RouterLink } from '@angular/router';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, SearchComponent, RouterLink],
+  imports: [CommonModule, SearchComponent, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
