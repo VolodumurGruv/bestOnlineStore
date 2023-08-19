8eb0af3e9f99ec6213576efbcb7bfaf56f574187
VolodumurGrushka <gru.v@i.ua>
2023-08-19 19:15:22 +0300

    get produc and get product by id in product service added

--- front-end/src/app/shared/services/products.service.ts
+++ front-end/src/app/shared/services/products.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../../interfaces/product.interfaces';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
+  private readonly URL: string = 'http://4200/api';
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.URL}/products`);
+  }
+
+  getProductById(id: string | number): Observable<Product> {
+    return this.http.get<Product>(`${this.URL}/products/${id}`);
+  }
 }
