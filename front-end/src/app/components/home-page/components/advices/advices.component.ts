ae49c73f409396607f15e258c941635ef656a25d
VolodumurGrushka <gru.v@i.ua>
2023-08-22 02:09:26 +0300

    changed the structure

---
+++ front-end/src/app/components/home-page/components/advices/advices.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-advices',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './advices.component.html',
+  styleUrls: ['./advices.component.scss']
+})
+export class AdvicesComponent {
+
+}
