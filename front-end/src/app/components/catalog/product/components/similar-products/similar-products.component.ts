26fca99fd71f6042882c5b412338505ff100fc54
shytuleva24 <shutyleva@gmail.com>
2023-08-24 14:27:19 +0300

    correctet similar-products

--- front-end/src/app/components/catalog/product/components/similar-products/similar-products.component.ts
+++ front-end/src/app/components/catalog/product/components/similar-products/similar-products.component.ts
@@ -2,13 +2,15 @@ import {Component, Input} from '@angular/core';
 
 import {Product} from "../../../../../interfaces/product.interfaces";
 import {NgIf} from "@angular/common";
+import {ProductCardComponent} from "../../../../../shared/product-card/product-card.component";
 
 @Component({
   selector: 'app-similar-products',
   standalone: true,
   templateUrl: './similar-products.component.html',
   imports: [
-    NgIf
+    NgIf,
+    ProductCardComponent
   ],
   styleUrls: ['./similar-products.component.scss']
 })
