b20dc83c557f5f4afcca7e57ead4a6a5ebb6ca33
MarkAlexI <markovaleksandr108@gmail.com>
2023-10-20 19:48:01 +0300

    Added test for getting user by id. Changed logger behavior for testing.

--- back-end/src/utils/logger.js
+++ back-end/src/utils/logger.js
@@ -1,17 +1,26 @@
 import winston from 'winston';
 
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.printf(({ timestamp, level, message }) => {
-      return `${timestamp} [${level.toUpperCase()}]: ${message}`;
-    })
-  ),
-  transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: 'error.log', level: 'error' })
-  ]
-});
+let logger = null;
+
+if (process.env.TEST_ENV !== 'test') {
+  logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.printf(({ timestamp, level, message }) => {
+        return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+      })
+    ),
+    transports: [
+      new winston.transports.Console(),
+      new winston.transports.File({ filename: 'error.log', level: 'error' })
+    ]
+  });
+} else {
+  logger = winston.createLogger({
+    level: 'none',
+    transports: []
+  });
+}
 
 export default logger;
--- back-end/tests/userRouter.test.js
+++ back-end/tests/userRouter.test.js
@@ -2,13 +2,29 @@ import chai from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../src/server.js';
 
+process.env.TEST_ENV = 'true';
+
 chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('User Routes', function() {
+  let userId = null;
+
   it('should get all users', function(done) {
     chai.request(app)
       .get('/api/user/all')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        userId = res._body.payload[0]._id;
+
+        done();
+      });
+  });
+
+  it('should get user by id', function(done) {
+    chai.request(app)
+      .get(`/api/user/${userId}`)
       .end((err, res) => {
         expect(res).to.have.status(200);
         expect(res).to.be.json;
